66bac981292adc529f8a9fb528c9541bbf51b8dd
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-09 11:46:54 +0530

    Re-organized all the folders, and started YouTube lectures series.

---
+++ YouTube Lectures/hello-world.js
@@ -0,0 +1,11 @@
+var a;
+var b;
+var c;
+a = 10;
+b = true;
+c = "Hello";
+function add(a, b) {
+    return a + b;
+}
+var sum = add(1, 2);
+console.log(sum);
---
+++ YouTube Lectures/hello-world.ts
@@ -0,0 +1,20 @@
+var a: number
+
+var b: boolean
+
+var c: string
+
+
+a = 10
+
+b = true
+
+c = "Hello"
+
+
+function add(a : number, b : number) {
+    return a + b
+}
+
+var sum = add(1, 2)
+console.log(sum)
